5ed7f6642c08f0cb55647bdb9ae4f7319fc741a3
Artemy <artegoser@gmail.com>
2024-02-29 15:40:18 +0300

    add index.json and readme updater

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # My blog posts
+
+Links currentrly not working
+
+- [Welcome to modern web](welcome-to-modern-web)
+- [AnoPaper: Story about how I created the notes service](anopaper-story-about-how-i-created-the-notes-service)
+- [ONLang: json based programming language](onlang-json-based-programming-language)
---
+++ index.js
@@ -0,0 +1,33 @@
+const metadataParser = require("markdown-yaml-metadata-parser");
+const fs = require("fs");
+
+let index = [];
+
+// Get all files in ./posts
+const files = fs.readdirSync("./posts");
+
+// Loop through each file
+for (const file of files) {
+  // Read the file
+  const content = fs.readFileSync(`./posts/${file}`, "utf8");
+
+  // Parse the metadata
+  let metadata = metadataParser(content).metadata;
+
+  metadata.id = file.replace(/\.md$/, "");
+
+  index.push(metadata);
+}
+
+index.sort((a, b) => {
+  return new Date(b.datePublished) - new Date(a.datePublished);
+});
+
+fs.writeFileSync("./index.json", JSON.stringify(index, null, 2));
+
+fs.writeFileSync(
+  "./README.md",
+  `# My blog posts\n\nLinks currentrly not working\n\n${index
+    .map((post) => `- [${post.title}](${post.slug})`)
+    .join("\n")}`
+);
